52f035f212ad03df5defd8a6617456cea02e8a38
Oleksii Stepchuk <34480855+sirLightRain@users.noreply.github.com>
2023-08-16 22:12:55 +0300

    +

--- src/components/task_3/FriendListItem.js
+++ src/components/task_3/FriendListItem.js
@@ -2,7 +2,7 @@ export const FriendListItem = ({ friends }) => {
   const { avatar, name, isOnline, id } = friends;
   return (
     <li className="item" key={id}>
-      <span className="status">{isOnline}isOnline</span>
+      <span className="status">{isOnline}isOnline ?</span>
       <img className="avatar" src={avatar} alt="User avatar" width="48" />
       <p className="name">{name}</p>
     </li>
