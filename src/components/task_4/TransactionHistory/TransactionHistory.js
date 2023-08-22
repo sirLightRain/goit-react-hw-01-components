da11a9b67a18de2f45ab6bbd1268380e3b217497
Oleksii Stepchuk <34480855+sirLightRain@users.noreply.github.com>
2023-08-22 12:19:27 +0300

    остання правка помилки

--- src/components/task_4/TransactionHistory/TransactionHistory.js
+++ src/components/task_4/TransactionHistory/TransactionHistory.js
@@ -5,25 +5,27 @@ import PropTypes from 'prop-types';
 export const TransactionHistory = ({ item }) => {
   return (
     <Table>
+      <thead>
+        <tr>
+          <TitleCell>Type</TitleCell>
+          <TitleCell>Amount</TitleCell>
+          <TitleCell>Currency</TitleCell>
+        </tr>
+      </thead>
 
-        <thead>
-          <tr>
-            <TitleCell>Type</TitleCell>
-            <TitleCell>Amount</TitleCell>
-            <TitleCell>Currency</TitleCell>
-          </tr>
-        </thead>
-  
-        <tbody>
-          {item.map(transaction => (
-            <TransHistEl key={transaction.id} item={transaction} />
-          ))}
-        </tbody>
-
+      <tbody>
+        {item.map(transaction => (
+          <TransHistEl key={transaction.id} item={transaction} />
+        ))}
+      </tbody>
     </Table>
   );
 };
 
 TransactionHistory.propTypes = {
-  item: PropTypes.array.isRequired,
+  item: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
